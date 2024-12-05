3d9a341c19fb0348c48c9ce628af7449ae5dea13
Meet Shah <meet30012002@gmail.com>
2024-12-05 15:28:40 -0500

    Firebase Setup

--- firebase.js
+++ firebase.js
@@ -4,7 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
+    apiKey: "AIzaSyCPierdy6hgaTynQA2cKJ6gxOBNxZDyrnA",
+    authDomain: "sem3advtopicslab3.firebaseapp.com",
+    databaseURL: "https://sem3advtopicslab3-default-rtdb.firebaseio.com",
+    projectId: "sem3advtopicslab3",
+    storageBucket: "sem3advtopicslab3.firebasestorage.app",
+    messagingSenderId: "931569171718",
+    appId: "1:931569171718:web:83efbd454736e925ca7287",
+    measurementId: "G-MHM1GN8CW8"
   };
 
 const app = initializeApp(firebaseConfig);
